2c465a154b5e8b2ad7fa4d8bad162cf790022fca
Michael Jähn <mjaehn@ethz.ch>
2023-04-12 13:58:28 +0200

    Add label

--- plot_clm_ts.py
+++ plot_clm_ts.py
@@ -1,12 +1,8 @@
-
-# Define netcdf filename
-
 import numpy as np
 import netCDF4
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.basemap import Basemap
-from tqdm import tqdm
 
 # Define the netCDF filename
 nc_file = '/scratch/snx3000/mjaehn/CH2025/spice/chain/work/cordex_12km_era5_gpu_20230317/post/1985_07/CLCT_ts.nc'
@@ -36,6 +32,18 @@ with netCDF4.Dataset(nc_file, 'r') as ds:
     # Define the contour levels for the plot
     levels = np.arange(0, 101, 10)
 
+    x, y = proj(lon_var[:], lat_var[:])
+
+    # Define the contour levels and labels
+    clevs = np.arange(0, 110, 10)
+    clabels = ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']
+
+    # Add a colorbar and legend
+    cs = proj.contourf(x, y, clct_var[0, :, :], levels=clevs, cmap='Blues_r', extend='max')
+    cbar = plt.colorbar(cs, ax=ax, orientation='horizontal')
+    cbar.set_label('Cloud Cover (%)')
+    legend = ax.legend(loc='lower left', bbox_to_anchor=(0.0, -0.4), ncol=6, frameon=False)
+
     # Define the function to animate the plot
     def animate(i):
         # Clear the previous plot
@@ -48,13 +56,11 @@ with netCDF4.Dataset(nc_file, 'r') as ds:
         clct = np.where(clct == fill_value, np.nan, clct)
 
         # Plot the data on the map
-        x, y = proj(lon_var[:], lat_var[:])
-        clevs = np.arange(0, 110, 10)
         cs = proj.contourf(x, y, clct, levels=clevs, cmap='Blues_r', extend='max')
         proj.drawcoastlines()
         proj.drawcountries()
-        proj.drawmeridians(np.arange(lon_min, lon_max, 10), labels=[False,False,False,True])
-        proj.drawparallels(np.arange(lat_min, lat_max, 10), labels=[True,False,False,False])
+        proj.drawmeridians(np.arange(-40, 50, 10), labels=[False,False,False,True])
+        proj.drawparallels(np.arange(30, 70, 10), labels=[True,False,False,False])
 
         # Add a title to the plot
         ax.set_title(f'{long_name} at time {i} ({time_var[i]})')
